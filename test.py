373067f962f46b59bf7c0d267628bb16cb988597
Juan Burtet <juan.burtet@gmail.com>
2019-11-27 16:50:48 -0300

    feito a analise dos resultados

--- test.py
+++ test.py
@@ -1,32 +1,89 @@
-import statistics as st
+# import os
 
-filename = "tests/rewards_for_simulations.csv"
+# base = "information/Resultados - TCC"
 
-labels = [10, 25, 50, 100, 250, 500]
+# folders = [os.path.join(base, o) for o in os.listdir(base) if os.path.isdir(os.path.join(base,o))]
 
-data = {
-    i:[] for i in labels
-}
+# for fold in folders:
+#     with open(base + "/1.txt", "a+") as f1, \
+#             open(base + "/2.txt", "a+") as f2, \
+#             open(base + "/3.txt", "a+") as f3:
 
-with open(filename, "r+") as f:
-    lines = f.readlines()
+#         # Arquivos de escrita
+#         files = [f1, f2, f3]
 
-    for i, line in enumerate(lines):
-        if i == 0:
-            continue
+#         for i in [1,2,3]:
+#             filename = "%s/%d.txt" % (fold, i)
 
-        info = line.split(",")
+#             f = open(filename, "r+")
+#             string = f.read()
+            
+#             files[i-1].write(string)
+#             f.close()
 
-        info = info[1:]
+#             pass
+#         pass
 
-        for i, a in enumerate(info):
-            data[labels[i]].append(float(a))
+#     pass
 
-            pass
+# import os
 
-    pass
+# base = "information/Resultados - TCC"
 
-print("Desvio padrão para cada simulação:")
-for key, value in data.items():
-    dp = st.stdev(value)
-    print("%s: %f" % (key, dp))
+# with open(base + "/1.txt", "r+") as f1, \
+#         open(base + "/2.txt", "r+") as f2, \
+#         open(base + "/3.txt", "r+") as f3:
+
+#     rewards = []
+#     files = [f1, f2, f3]
+
+#     for f in files:
+#         lines = f.readlines()
+
+#         data = {
+#             "min": 1,
+#             "max": 0,
+#             "media": None,
+#             "count": 0,
+#             "wins": 0
+#         }
+
+#         r = 0
+#         count = 0
+#         for line in lines:
+#             value = float(line)
+
+#             if value == 1.0:
+#                 data['wins'] += 1
+
+#             if data["min"] > value:
+#                 data['min'] = value
+            
+#             if data['max'] < value:
+#                 data['max'] = value
+
+#             r += value
+#             count += 1
+
+#         data['media'] = r/count
+#         data['count'] = count
+
+#         rewards.append(data)
+
+# for r in rewards:
+#     print(r)
+
+
+import quebra_doce_bot as bot
+
+
+maps = [
+    "levels/75_75_points_protection_objective.csv",
+    "levels/85_85_points_protection_objective.csv",
+    "levels/95_95_points_protection_objective.csv"
+]
+
+for m in maps:
+
+    ia = bot.QuebraDoceAI(file=m)
+    print(ia.do_playouts(info=False, final=True))
